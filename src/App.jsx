40ce35ea2a012bbbcb8ce41fc1671bb364cc5797
Paboda97 <pabo97sm@gmail.com>
2024-10-27 22:42:57 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './comp/Header.jsx';
-import Navbar from "./comp/Navbar.jsx";
+import Navbar from './comp/Navbar.jsx';
 import Bodyone from './comp/Bodyone.jsx';
 
 const App = () => {
